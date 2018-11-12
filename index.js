179152797acba34603a69f7ee78a303039c064c5
Cem Torun <cemtorun283@gmail.com>
2018-11-12 18:45:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,34 +26,15 @@ exports.handler = (event, context, callback) => {
 };
 
 function parseJSON(event, context, callback){
-    //Do the actual work here
-    // console.log(fileData.Body.toString())
     const spawnSync = require("child_process").spawnSync;
     const pythonProcess = spawnSync('python',["./main.py", fileData.Body.toString()]);
     var checkValue;
-    // pythonProcess.stdout.on('data', function(data) {
-    //     // res.send(data.toString());
-    //     console.log(data.toString())
-    // } )
-    // pythonProcess.stdout.on('data', (data) => {
-    //     console.log("hrloooo")
-    //     checkValue = data.toString();
-    //     console.log(checkValue)
-    //     callback(null, {
-    //         'statusCode': 200,
-    //         'headers': {'Content-Type': 'application/json'},
-    //         // 'body': fileData.Body.toString()
-    //         'body': checkValue
-    //     }) //Then end
-
-    // });
     console.log(pythonProcess)
     console.log(pythonProcess.stderr.toString())
     console.log(pythonProcess.stdout.toString())
     return callback(null,  {
         'statusCode': 200,
         'headers': {'Content-Type': 'application/json'},
-        // 'body': fileData.Body.toString()
         'body': true
     })
 
